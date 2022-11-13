dff543dfdb1b39dd19eff7de5cc4257620fc2df1
bmv000 <106543741+bmv000@users.noreply.github.com>
2022-11-13 18:34:34 +0200

    done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,11 @@
-
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
 
 import Notiflix from 'notiflix';
 
@@ -40,4 +47,5 @@ function createPromise(position, delay) {
         });
       }
 });
+
 
