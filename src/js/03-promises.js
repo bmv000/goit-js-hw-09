4f0b90ad90e6f57a460e09ce45cb4e56fa3cf224
bmv000 <106543741+bmv000@users.noreply.github.com>
2022-11-13 17:45:23 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,4 @@
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+
 
 import Notiflix from 'notiflix';
 
@@ -47,5 +40,4 @@ function createPromise(position, delay) {
         });
       }
 });
-
 
